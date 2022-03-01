be5e846af1acae0106549c50a21e01f3c4ef1f70
Donatella7 <campaniellodonatella@gmail.com>
2022-03-01 15:40:37 +0100

    array img, country and text

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+const arraySmallImg = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
+
+const titleBigImg = ["Svezia","Svizzera","Gran Bretagna","Germania","Paradise"];
+
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
