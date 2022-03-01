5355b90a53ad723c7385bb12e30052ccb121294a
Donatella7 <campaniellodonatella@gmail.com>
2022-03-01 16:12:50 +0100

    add tag img and its container to the html

--- js/myscript.js
+++ js/myscript.js
@@ -10,3 +10,20 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+//# prendo il container al quale andrò aggiungere ogni elemento dell'array "arraySmallImg"
+const carouselContainer = document.querySelector(".my-carousel-container");
+
+//% creo una variabile alla quale va inserito l'elemento dell'array(arraySmallImg) all'interno del tag <div>
+let carouselContent = '';
+
+// ! inserico gli elementi  
+for (let i = 0 ; i < arraySmallImg.length ; i++){
+    carouselContent += `
+    <div class="carousel-element">
+        <img src="${arraySmallImg[i]}" class="my-small-img" alt="Random picture">
+    </div>`
+}
+// £ stampo gli elementi costruiti nel ciclo for nel file html
+carouselContainer.innerHTML += carouselContent;
+/* carouselContent.classList.add("my-small-img"); */
